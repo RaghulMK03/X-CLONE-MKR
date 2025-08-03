7bc89c56e806050ba0d958ba0315777429432285
RaghulMK03 <mailtooraghul@gmail.com>
2025-08-03 19:43:01 +0530

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 import express from "express";
+import cors from  "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./routes/user.route.js";
+
+
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
 const app = express();
 
-//to start the db
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
 // connectDB();
 
 app.get("/", (req, res) => res.send("Hello from server"));
+
+app.use("/api/users", userRoutes);
 
 //to start the server 
 const startServer = async () => {
